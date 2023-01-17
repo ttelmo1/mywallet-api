d1c3a059ba15bb9ed24f32019f49d5154b6b9424
ttelmo1 <telmobsj@gmail.com>
2023-01-17 19:14:25 -0300

    rota sign-in

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br.js';
 import { MongoClient } from 'mongodb';
 
+const userSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.required(),
+    repeatPassword: Joi.ref('password')
+});
+
+
 dotenv.config();
 const server = express();
 server.use(cors());
@@ -13,14 +21,39 @@ server.use(express.json());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
-try{
+try {
     await mongoClient.connect();
     db = mongoClient.db();
     console.log('Conectado ao banco de dados');
 }
-catch(error){
+catch (error) {
     console.log('Erro ao conectar ao banco de dados');
 }
+
+server.post('/sign-in', async (req, res) => {
+    const { name, email, password } = req.body;
+    const result = userSchema.validate({ name, email, password });
+    if (result.error) {
+        return res.status(400).send(result.error.details[0].message);
+    }
+    try {
+        const user = await db.collection('users').findOne({ email });
+        if (user) {
+            return res.sendStatus(409);
+        }
+        await db.collection('users').insertOne({ name, email, password });
+        res.sendStatus(201);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+});
+
+
+
+
 
 const PORT = 5000;
 server.listen(PORT, () => {
